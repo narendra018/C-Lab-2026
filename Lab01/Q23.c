f8c2e4b9dac90ad7ed9b7a6c9b2f9fc5a4b5786d
narendra018 <narendrakumar25467@gmail.com>
2026-09-19 16:51:10 +0530

    Updated project file

--- Lab01/Q23.c
+++ Lab01/Q23.c
@@ -3,9 +3,17 @@ int main(){
     int n,exp;
     printf("Enter the number and exponent");
     scanf("%d %d", &n, &exp);
-    int k=1;
-    for(int i=1;i<=exp;i++){
-        k=k*n;
+    int m=-exp;
+    float k=1;
+    if(exp>0){
+        for(int i=1;i<=exp;i++){
+            k=k*n;
+        }
     }
-    printf("req no is %d",k);
+    if(exp<0){
+        for(int i=1;i<=m;i++){
+            k=k*(1/(float)n);
+        }
+    }
+    printf("req no is %.3f",k);
 }
